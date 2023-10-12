a795647a1ed0357d409e6a5a9be469ba982c6ba4
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-10-12 01:46:31 +0000

    Ran black

--- volume_surface_area_right_cylinder.py
+++ volume_surface_area_right_cylinder.py
@@ -2,65 +2,73 @@
 
 # Created by: Lily Carroll
 # Created on: Sept/25/2023
-# This program calculates the volume and the surface area of a right cylinder with user 
+# This program calculates the volume and the surface area of a right cylinder with user
 # input.
-# Importing the math module and my constants from my constants.py file. 
+# Importing the math module and my constants from my constants.py file.
 import math
 import constants
 
+
 def main():
-  # Explaining my program to the user.
-  print("This program will calculates the surface area and volume of a right cylinder.")
-  
-  # Telling user the 3 options they can choose from for their calculation.
-  print("The units of calculation that you may choose from are:") 
-  print("1 - centimeters")
-  print("2 - millimeters")
-  print("3 - meters")
+    # Explaining my program to the user.
+    print(
+        "This program will calculates the surface area and volume of a right cylinder."
+    )
 
-  # Getting the userinput for the units.
-  unit_choice = input(
-    "Please enter the number representing the unit of measurement that you would like to use: ")
-  
-  # Using if then, elif and else to allow user to choose from the unit options.
-  # If the units chosen are cm, then do the calculations using centimeters.
-  if unit_choice == "1":
-    unit = constants.UNIT_CENTIMETERS
+    # Telling the user the 3 options they can choose from for their calculation.
+    print("The units of calculation that you may choose from are:")
+    print("1 - centimeters")
+    print("2 - millimeters")
+    print("3 - meters")
 
-  # Else if the units chosen are mm, then do the calculations using millimeters.
-  elif unit_choice == "2":
-    unit = constants.UNIT_MILLIMETERS
+    # Getting the user input for the units.
+    unit_choice = input(
+        "Please enter the number representing the unit of measurement that you would like to use: "
+    )
 
-  # Else if the units chosen are m, then do the calculations using meters. 
-  elif unit_choice == "3":
-    unit = constants.UNIT_METERS
+    # Using if then, elif and else to allow the user to choose from the unit options.
+    # If the units chosen are cm, then do the calculations using centimeters.
+    if unit_choice == "1":
+        unit = constants.UNIT_CENTIMETERS
 
-  # Else display the error message that the units chosen are not valid.
-  else:
-    print("Invalid units. Please select one from the options above.")
-    exit()
+    # Else if the units chosen are mm, then do the calculations using millimeters.
+    elif unit_choice == "2":
+        unit = constants.UNIT_MILLIMETERS
 
-    # Declaring variables.
-  radius = float(input("Enter a radius of a right cylinder: "))
-  height = float(input("Enter a height of a right cylinder: "))
+    # Else if the units chosen are m, then do the calculations using meters.
+    elif unit_choice == "3":
+        unit = constants.UNIT_METERS
 
-# Calculating the volume and surface area.
-  volume = math.pi * radius ** 2 * height
-  surface_area = 2 * math.pi * radius * height + 2 * math.pi * radius ** 2
+    # Else display the error message that the units chosen are not valid.
+    else:
+        print("Invalid units. Please select one from the options above.")
+        exit()
 
-# Error checking for a input of 0, using an if statement.
-  if radius == 0 or height == 0:
-    print("Please enter a valid number")
+        # Declaring variables.
+    radius = float(input("Enter a radius of a right cylinder: "))
+    height = float(input("Enter a height of a right cylinder: "))
 
-# Error checking for a negative number input using an elif statement.
-  elif radius < 0 or height < 0:
-    print("Please enter a valid number")
-    
-# Printing the volume and surface area of the cylinder.
-  else:
-    print("The volume of the right cylinder is = {:.2f} {}^3". format (volume, unit))
-    print("The surface area of the right cylinder is = {:.2f} {}^2". format(surface_area, unit))
+    # Calculating the volume and surface area.
+    volume = math.pi * radius**2 * height
+    surface_area = 2 * math.pi * radius * height + 2 * math.pi * radius**2
+
+    # Error checking for an input of 0, using an if statement.
+    if radius == 0 or height == 0:
+        print("Please enter a valid number")
+
+    # Error checking for a negative number input using an elif statement.
+    elif radius < 0 or height < 0:
+        print("Please enter a valid number")
+
+    # Printing the volume and surface area of the cylinder.
+    else:
+        print("The volume of the right cylinder is = {:.2f} {}^3".format(volume, unit))
+        print(
+            "The surface area of the right cylinder is = {:.2f} {}^2".format(
+                surface_area, unit
+            )
+        )
 
 
 if __name__ == "__main__":
-  main()
+    main()
